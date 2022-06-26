f58719236ca8e32942914a1f5ed1457a6bec83ed
Djalma Junior <djalmajunior03@outlook.com>
2022-06-26 07:42:10 -0300

    testes com stdout e stdin + emissao de eventos

--- 03-events/index.js
+++ 03-events/index.js
@@ -10,6 +10,9 @@ const emissor = Emissor.client
 // }, 1000)
 
 const stdin = process.openStdin()
+process.stdout.write('Para cancelar use ctrl+c a qualquer momento.\n')
+process.stdout.write('Informe o local de click.\n')
+
 
 stdin.addListener('data', (value) => {
   emissor.emit(evento, value.toString().trim())
